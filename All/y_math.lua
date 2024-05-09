99f73516b73ce536fce8d3dbc01dc57e2d8764fb
steve-account <muhandasrazil@gmail.com>
2024-05-09 15:22:11 -0400

    modified:   All/y_math.lua 	modified:   PC/y_math.lua 	modified:   Turtle/y_math.lua

--- All/y_math.lua
+++ All/y_math.lua
@@ -8,6 +8,7 @@ trtl_return = read()
 term.clear()
 term.setCursorPos(1,1)
 
+
 trtl_loc = {x = actions.pcTable[17].location.x, y = actions.pcTable[17].location.y, z = actions.pcTable[17].location.z}
 pc_loc = {x = actions.pcTable[pc_selected].location.x, y = actions.pcTable[pc_selected].location.y, z = actions.pcTable[pc_selected].location.z}
 trtl_ori = actions.pcTable[17].orientation
@@ -17,36 +18,27 @@ dist_pc = {}
 
 
 function dist_output()
-    --calibrate distance of target
     dist_x = math.abs(math.max(trtl_loc.x,pc_loc.x)-math.min(trtl_loc.x,pc_loc.x))
     dist_y = math.abs(math.max(trtl_loc.y,pc_loc.y)-math.min(trtl_loc.y,pc_loc.y))
     dist_z = math.abs(math.max(trtl_loc.z,pc_loc.z)-math.min(trtl_loc.z,pc_loc.z))
-
     dist_pc = {x = dist_x, y = dist_y, z = dist_z}
-
 end
-
-
 dist_output()
+
+
 
 print(trtl_loc.x,trtl_loc.y,trtl_loc.z)
 print(pc_loc.x,pc_loc.y,pc_loc.z)
---dist_x = math.abs(math.max(trtl_loc.x,pc_loc.x)-math.min(trtl_loc.x,pc_loc.x))
---dist_y = math.abs(math.max(trtl_loc.y,pc_loc.y)-math.min(trtl_loc.y,pc_loc.y))
---dist_z = math.abs(math.max(trtl_loc.z,pc_loc.z)-math.min(trtl_loc.z,pc_loc.z))
-
 print("x: "..dist_pc.x.." y: "..dist_pc.y.." z: "..dist_pc.z)
+
 
 if dist_x >= dist_z then
     xyz_priority = 'zxy'
 else
     xyz_priority = 'xzy'
 end
-
 print(xyz_priority)
 
---print("calibrate pc orientation? y/n")
---usr_go = read()
 
 if usr_go == 'y' then
     term.clear()
@@ -100,6 +92,7 @@ if usr_go == 'y' then
     end
 end
 
+
 if trtl_return == 'y' then
     actions.go_to(trtl_loc,trtl_ori,'xyz',true)
     actions.calibrate_turtle()
@@ -108,5 +101,4 @@ else
     actions.calibrate_turtle()
     actions.updateAndBroadcast()
 end
-
 print("done!")
